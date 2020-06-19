ed02a7c2962912d24dcaca7d529e9e12616236f0
Oleh <olegflud5@gmail.com>
2020-06-20 00:23:25 +0300

    remove dist

--- dist/constants/action.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ActionEnum = void 0;
-var ActionEnum;
-(function (ActionEnum) {
-    ActionEnum["USER_REGISTER"] = "user_register";
-})(ActionEnum = exports.ActionEnum || (exports.ActionEnum = {}));
-//# sourceMappingURL=action.enum.js.map
--- dist/helpers/passwordHasher.helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkPassword = exports.hashPassword = void 0;
-const bcrypt = require("bcrypt");
-exports.hashPassword = (password) => bcrypt.hash(password, 10);
-exports.checkPassword = (password, hash) => bcrypt.compare(password, hash);
-//# sourceMappingURL=passwordHasher.helper.js.map
